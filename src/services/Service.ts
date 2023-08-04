9ee8b6d1dba6a55d954c7d4b44807df202d15788
Gabriele <cristiine94@outlook.com>
2023-08-04 15:23:09 -0300

    Update: atualizando o projeto - @GabrieleeCris

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://blogdothiagofaccipieri.onrender.com/'
+  baseURL: 'https://blogpessoal-ju1g.onrender.com/'
 })
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
