f566681fbc1372a014b11657688c2cf7bc301689
Gabriele <cristiine94@outlook.com>
2023-08-12 10:47:13 -0300

    Atualizando projeto - @GabrieleeCris

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ import { AuthContext } from '../../contexts/AuthContext'
 
 function Footer() {
 
-  const { usuario, handleLogout } = useContext(AuthContext)
+  const { usuario} = useContext(AuthContext)
 
   let footerComponent
 
